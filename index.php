aea1af2b04e75d86a8c3261cbd3a1fce1394fbce
nicolassbon <nicolasbon019@gmail.com>
2025-07-01 16:41:51 -0300

    validacion para las rutas del editor

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 session_start();
 
+// Error que hacia que el editor saliera siempre
+// Si el servidor pide el favicon, se ignora
+if ($_SERVER['REQUEST_URI'] === '/favicon.ico') {
+    http_response_code(204);
+    exit();
+}
+
 // Rutas accesibles sin estar logueado
 $rutasPublicas = [
     'login/show',
@@ -24,28 +31,36 @@ $method = $_GET['method'] ?? null;
 
 $ruta = "$controller/$method";
 
+// Validacion General
 if (!isset($_SESSION['usuario_id']) && !in_array($ruta, $rutasPublicas, true)) {
     header("Location: /");
     exit();
 }
 
+// Validacion por Roles
 if (isset($_SESSION['usuario_id'])) {
     $roles = $_SESSION['roles'] ?? [];
 
-    // Si la ruta empieza con 'editor/', solo los editores pueden acceder
+    // Si la ruta es de editor y el usuario NO tiene el rol, lo sacamos.
     if (str_starts_with($ruta, 'editor/') && !in_array('editor', $roles, true)) {
-        header("Location: /lobby/show");
+        error_log("Entro en primero");
+        session_unset();
+        session_destroy();
+        header("Location: /login/show");
         exit();
     }
 
-    // Si el usuario es EDITOR, solo puede acceder a rutas que empiezan con 'editor/'
-    // Tambien puede acceder a las rutas publicas
-    if (in_array('editor', $roles, true)) {
-        if (!str_starts_with($ruta, 'editor/') && !in_array($ruta, $rutasPublicas, true)) {
-            header("Location: /editor/show");
-            exit();
-        }
+    if (in_array('editor', $roles, true) &&
+        !str_starts_with($ruta, 'editor/') &&
+        !in_array($ruta, $rutasPublicas, true)
+        ) {
+        error_log("Entro en segundo, " . $ruta);
+        session_unset();
+        session_destroy();
+        header("Location: /login/show");
+        exit();
     }
+
 }
 
 require_once "Configuration.php";
